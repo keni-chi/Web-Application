ad83639df6e611ef9cb72b2118622ccdb126ee60
keni-chi <neko9dog9access@gmail.com>
2019-04-09 00:00:42 +0900

    commit

--- MySQL/transaction.sql
+++ MySQL/transaction.sql
@@ -3,9 +3,15 @@ id int(11) AUTO_INCREMENT NOT NULL,
 val int(11) DEFAULT NULL,
 constraint ID_PK primary key (id)
 );
-
 desc sample;
+
 
 select @@autocommit;
 set autocommit = 0;
-select @@autocommit;
+
+insert into sample(val) values(10),(20),(30);
+start transaction;
+select * from sample;
+truncate table sample;
+rollback;
+select * from sample;
