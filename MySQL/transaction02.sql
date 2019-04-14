a9c683545b13fff69eab198398ac4b9d961b5282
keni-chi <neko9dog9access@gmail.com>
2019-04-14 19:46:55 +0900

    commit

--- MySQL/transaction02.sql
+++ MySQL/transaction02.sql
@@ -5,3 +5,8 @@ price int(11) DEFAULT NULL,
 constraint ID_PK primary key (product_id)
 );
 desc products;
+
+
+insert ignore into products values (15, 'ipos', 23000);
+select * from products where product_id = 15 lock in share mode;
+select * from products where product_id = 15 for update;
